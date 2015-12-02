c6d8b80881f9c9ea47128db56a8338c21f7cda2a
KASHIMA, Kazuo <k4200@kazu.tv>
2015-12-02 16:25:56 +0900

    Bump to 3.0.4-SNAPSHOT

--- project/Common.scala
+++ project/Common.scala
@@ -1,7 +1,7 @@
 
 
 object Common {
-  def version = "3.0.3"
+  def version = "3.0.4-SNAPSHOT"
   def playVersion = System.getProperty("play.version", "2.3.7")
   def scalaVersion =  System.getProperty("scala.version", "2.11.1")
 }
