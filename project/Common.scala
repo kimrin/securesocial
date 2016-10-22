7e9de8412cfa58d1348311e2aa77838a68aa5af1
KASHIMA, Kazuo <k4200@kazu.tv>
2016-10-22 13:49:17 +0900

    Bump to 3.2.0; play version 2.5.8

--- project/Common.scala
+++ project/Common.scala
@@ -1,9 +1,9 @@
 
 
 object Common {
-  def version = "3.2.0-SNAPSHOT"
+  def version = "3.2.0"
 
-  def playVersion = System.getProperty("play.version", "2.5.1")
+  def playVersion = System.getProperty("play.version", "2.5.8")
 
   def scalaVersion = System.getProperty("scala.version", "2.11.8")
 }
