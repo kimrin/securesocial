fe7c31ae645735120380f6f03beed26450f3a7a9
KASHIMA, Kazuo <k4200@kazu.tv>
2016-12-09 00:43:06 +0900

    Bump Play framework version to 2.5.10

--- project/Common.scala
+++ project/Common.scala
@@ -3,7 +3,7 @@
 object Common {
   def version = "3.2.1-SNAPSHOT"
 
-  def playVersion = System.getProperty("play.version", "2.5.8")
+  def playVersion = System.getProperty("play.version", "2.5.10")
 
   def scalaVersion = System.getProperty("scala.version", "2.11.8")
 }
