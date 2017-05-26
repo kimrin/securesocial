7d1ab82f36d8f299dcbdfb015fc86004eac3776e
KASHIMA Kazuo <k4200@kazu.tv>
2017-05-26 11:37:37 +0900

    Update play deps to 2.5.15

--- project/Common.scala
+++ project/Common.scala
@@ -3,7 +3,7 @@
 object Common {
   def version = "3.2.4-SNAPSHOT"
 
-  def playVersion = System.getProperty("play.version", "2.5.10")
+  def playVersion = System.getProperty("play.version", "2.5.15")
 
   def scalaVersion = System.getProperty("scala.version", "2.11.8")
 }
